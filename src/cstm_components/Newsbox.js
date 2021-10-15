241f57d66ce885cbfe23d85f362ea1298cd547e3
Haider Ali <malikhaider0567@gmail.com>
2021-10-15 13:25:20 +0530

    Added loader to newsapp

--- src/cstm_components/Newsbox.js
+++ src/cstm_components/Newsbox.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import Newsitem from "./Newsitem";
+import Spinner from "./Spinner";
 
 export class Newsbox extends Component {
 	// constructor
 	constructor() {
 		super();
-    this.state = {
-      totalResults:0,
+		this.state = {
+			totalResults: 0,
 			articles: [],
 			loading: false,
 			page: 1,
@@ -15,39 +16,45 @@ export class Newsbox extends Component {
 
 	async componentDidMount() {
 		let url =
-			"https://newsapi.org/v2/top-headlines?country=in&apiKey=69823bafa652459a8eefdf6b1230054a&page=1&pageSize=20";
+			`https://newsapi.org/v2/top-headlines?country=in&apiKey=69823bafa652459a8eefdf6b1230054a&page=1&pageSize=${this.props.pageSize}`;
+		this.setState({ loading: true });
 		let data = await fetch(url);
 		let parsedData = await data.json();
-    this.setState({
-      totalResults: parsedData.totalResults,
+		this.setState({
+			totalResults: parsedData.totalResults,
 			articles: parsedData.articles,
 			page: 1,
+			loading: false,
 		});
 	}
 
 	handlePrevclick = async () => {
 		let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=69823bafa652459a8eefdf6b1230054a&page=${
 			this.state.page - 1
-		}&pageSize=20`;
+		}&pageSize=${this.props.pageSize}`;
+		this.setState({ loading: true });
 		let data = await fetch(url);
 		let parsedData = await data.json();
-    this.setState({
+		this.setState({
 			totalResults: parsedData.totalResults,
 			articles: parsedData.articles,
 			page: this.state.page - 1,
+			loading: false,
 		});
 	};
 
 	handleNextclick = async () => {
 		let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=69823bafa652459a8eefdf6b1230054a&page=${
 			this.state.page + 1
-		}&pageSize=20`;
+		}&pageSize=${this.props.pageSize}`;
+		this.setState({ loading: true });
 		let data = await fetch(url);
 		let parsedData = await data.json();
-    this.setState({
+		this.setState({
 			totalResults: parsedData.totalResults,
 			articles: parsedData.articles,
 			page: this.state.page + 1,
+			loading: false,
 		});
 	};
 
@@ -59,7 +66,7 @@ export class Newsbox extends Component {
 				}}
 			>
 				<h2 className="text-center my-3">Newsatonian - Top Headlines</h2>
-
+        {this.state.loading && <Spinner/>}
 				<div
 					className="row text-center"
 					style={{
@@ -68,24 +75,27 @@ export class Newsbox extends Component {
 						margin: "auto",
 					}}
 				>
-					{this.state.articles.map((element) => {
-						return (
-							<div key={element.url} className="col-md-3">
-								<Newsitem
-									title={element.title ? element.title.slice(0, 40) : ""}
-									description={
-										element.description ? element.description.slice(0, 80) : ""
-									}
-									urlToImage={
-										element.urlToImage
-											? element.urlToImage
-											: "https://www.euractiv.com/wp-content/uploads/sites/2/2014/03/news-default.jpeg"
-									}
-									url={element.url}
-								/>
-							</div>
-						);
-					})}
+					{!this.state.loading &&
+						this.state.articles.map((element) => {
+							return (
+								<div key={element.url} className="col-md-3">
+									<Newsitem
+										title={element.title ? element.title.slice(0, 40) : ""}
+										description={
+											element.description
+												? element.description.slice(0, 80)
+												: ""
+										}
+										urlToImage={
+											element.urlToImage
+												? element.urlToImage
+												: "https://www.euractiv.com/wp-content/uploads/sites/2/2014/03/news-default.jpeg"
+										}
+										url={element.url}
+									/>
+								</div>
+							);
+						})}
 					<div className="conatainer d-flex justify-content-evenly my-3 mb-4">
 						<button
 							onClick={this.handlePrevclick}
@@ -96,7 +106,9 @@ export class Newsbox extends Component {
 							&larr; Previous
 						</button>
 						<button
-							disabled={this.state.page >= Math.ceil(this.state.totalResults / 20)}
+							disabled={
+								this.state.page >= Math.ceil(this.state.totalResults / this.props.pageSize)
+							}
 							onClick={this.handleNextclick}
 							type="button mx-3"
 							className="btn btn-dark"
