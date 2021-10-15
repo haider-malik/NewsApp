960bb5104a03dca556382de6e1198ee6968243ac
Haider Ali <malikhaider0567@gmail.com>
2021-10-15 12:08:54 +0530

    Added previous and next buttons to newsatonian

--- src/cstm_components/Newsbox.js
+++ src/cstm_components/Newsbox.js
@@ -2,25 +2,54 @@ import React, { Component } from "react";
 import Newsitem from "./Newsitem";
 
 export class Newsbox extends Component {
-
 	// constructor
 	constructor() {
 		super();
-		this.state = {
+    this.state = {
+      totalResults:0,
 			articles: [],
 			loading: false,
+			page: 1,
 		};
-  }
-  
-  async componentDidMount() {
-    let url =
-      "https://newsapi.org/v2/top-headlines?country=in&apiKey=69823bafa652459a8eefdf6b1230054a";
-    let data = await fetch(url);
-    let parsedData = await data.json()
+	}
+
+	async componentDidMount() {
+		let url =
+			"https://newsapi.org/v2/top-headlines?country=in&apiKey=69823bafa652459a8eefdf6b1230054a&page=1&pageSize=20";
+		let data = await fetch(url);
+		let parsedData = await data.json();
     this.setState({
-      articles: parsedData.articles
-    })
-  }
+      totalResults: parsedData.totalResults,
+			articles: parsedData.articles,
+			page: 1,
+		});
+	}
+
+	handlePrevclick = async () => {
+		let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=69823bafa652459a8eefdf6b1230054a&page=${
+			this.state.page - 1
+		}&pageSize=20`;
+		let data = await fetch(url);
+		let parsedData = await data.json();
+    this.setState({
+			totalResults: parsedData.totalResults,
+			articles: parsedData.articles,
+			page: this.state.page - 1,
+		});
+	};
+
+	handleNextclick = async () => {
+		let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=69823bafa652459a8eefdf6b1230054a&page=${
+			this.state.page + 1
+		}&pageSize=20`;
+		let data = await fetch(url);
+		let parsedData = await data.json();
+    this.setState({
+			totalResults: parsedData.totalResults,
+			articles: parsedData.articles,
+			page: this.state.page + 1,
+		});
+	};
 
 	render() {
 		return (
@@ -31,20 +60,22 @@ export class Newsbox extends Component {
 			>
 				<h2 className="text-center my-3">Newsatonian - Top Headlines</h2>
 
-        <div
-						className="row text-center"
-						style={{
-							justifyContent: "center",
-							width: "100%",
-							margin: "auto",
-            }}
-        >
-        {this.state.articles.map((element) => {
+				<div
+					className="row text-center"
+					style={{
+						justifyContent: "center",
+						width: "100%",
+						margin: "auto",
+					}}
+				>
+					{this.state.articles.map((element) => {
 						return (
 							<div key={element.url} className="col-md-3">
 								<Newsitem
-									title={element.title? element.title.slice(0, 40): ""}
-									description={element.description? element.description.slice(0, 80):""}
+									title={element.title ? element.title.slice(0, 40) : ""}
+									description={
+										element.description ? element.description.slice(0, 80) : ""
+									}
 									urlToImage={
 										element.urlToImage
 											? element.urlToImage
@@ -54,9 +85,27 @@ export class Newsbox extends Component {
 								/>
 							</div>
 						);
-				})}
-        </div>
+					})}
+					<div className="conatainer d-flex justify-content-evenly my-3 mb-4">
+						<button
+							onClick={this.handlePrevclick}
+							disabled={this.state.page <= 1}
+							type="button mx-3"
+							className="btn btn-dark"
+						>
+							&larr; Previous
+						</button>
+						<button
+							disabled={this.state.page >= Math.ceil(this.state.totalResults / 20)}
+							onClick={this.handleNextclick}
+							type="button mx-3"
+							className="btn btn-dark"
+						>
+							Next &rarr;
+						</button>
+					</div>
 				</div>
+			</div>
 		);
 	}
 }
--- src/cstm_components/Newsitem.js
+++ src/cstm_components/Newsitem.js
@@ -5,18 +5,24 @@ export class Newsitem extends Component {
     let { title, description, urlToImage, url } = this.props;
 		return (
 			<div>
-        <div className="card my-3" style={{
-          maxWidth:"250px",
-          margin: "auto"
-        }}>
+				<div
+					className="card my-3"
+					style={{
+						maxWidth: "250px",
+						margin: "auto",
+					}}
+				>
 					<img src={urlToImage} className="card-img-top" alt="..." />
 					<div className="card-body">
 						<h5 className="card-title">{title}...</h5>
-						<p className="card-text">
-							{description}...
-						</p>
-						<a href={url} target="_blank" className="btn btn-primary">
-              Expand
+						<p className="card-text">{description}...</p>
+						<a
+							rel="noreferrer"
+							href={url}
+							target="_blank"
+							className="btn btn-primary"
+						>
+							Expand
 						</a>
 					</div>
 				</div>
